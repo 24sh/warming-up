09bdfc36aa4467f73a23adaf43a9a11cf58833bd
24sh <908988316@qq.com>
2015-10-28 14:31:42 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+
+gulp.task('sass', function(){
+  gulp.src('styles/main.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('css'));
+});
+gulp.task('prefix', function(){
+  gulp.src('styles/main.scss')
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(gulp.dest('css'));
+})
+gulp.task('watch', function() {
+  gulp.watch('styles/*.scss', ['sass'])
+})
+
